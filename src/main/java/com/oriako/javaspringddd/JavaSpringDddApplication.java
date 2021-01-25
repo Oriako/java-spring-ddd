18dbfe2e0c0f5216731feaed0b3d0ddd51552203
Oriako <daniel.orias.nieto@gmail.com>
2021-01-25 21:30:18 +0100

    project created

---
+++ src/main/java/com/oriako/javaspringddd/JavaSpringDddApplication.java
@@ -0,0 +1,13 @@
+package com.oriako.javaspringddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringDddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oriako/javaspringddd/JavaSpringDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oriako.javaspringddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
